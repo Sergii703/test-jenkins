bb244ceeb4c589db9e373bff83305525a39df454
Sergii703 <sergey703@gmail.com>
2020-03-20 21:03:18 +0000

    some changes

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,7 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
+echo "one more nice line"
 echo "One more line"
 echo "I just changed it"
 echo "This was added"
