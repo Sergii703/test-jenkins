8816a70af2be1ebb27a4033819b1a78b09cbc786
Sergii <sergey703@gmail.com>
2020-06-09 15:07:48 +0200

    Delete testscript.sh

--- testscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "this script is from github"
-NUMBER=$(($NUMBERONE + $NUMBERTWO))
-echo "$NUMBER"
-echo "one more nice line"
-echo "One more line"
-echo "I just changed it"
-echo "This was added"
